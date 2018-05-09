fdf0ad0e414f9c7670b8d6050a351b780e4e45b4
Tom Wyant <wyant@cpan.org>
2018-05-08 21:30:49 -0400

    Fix typo in license caused by too-promiscuous s///
    Basically, each Syntax filter was licensed under its name 5.10.0 (e.g.
    ::Cc under Cc 5.10.0). They are all now under Perl.

--- lib/App/AckX/Preflight/Syntax/_cc_like.pm
+++ lib/App/AckX/Preflight/Syntax/_cc_like.pm
@@ -178,8 +178,8 @@ Thomas R. Wyant, III F<wyant at cpan dot org>
 Copyright (C) 2018 by Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
-under the same terms as _cc_like 5.10.0. For more details, see the full
-text of the licenses in the directory LICENSES.
+under the same terms as Perl 5.10.0. For more details, see the full text
+of the licenses in the directory LICENSES.
 
 This program is distributed in the hope that it will be useful, but
 without any warranty; without even the implied warranty of
